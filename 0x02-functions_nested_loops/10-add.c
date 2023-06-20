e37f3f17f66b5db039a5cdd870bc75943051cfae
Siyabonga <siyamhlongo.p7@gmail.com>
2023-06-20 14:32:07 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - addition two integer a and b
+ *
+ * Description: adds two integers and returns the result.
+ * @a: is num integer
+ * @b: is integer
+ * Prototype: int add(int, int);
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+
+}
+
+
