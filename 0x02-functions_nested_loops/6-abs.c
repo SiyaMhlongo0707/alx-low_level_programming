4b4e0f5d4f18b9c4a2fd39a476aab2e5dc372f49
Siyabonga <siyamhlongo.p7@gmail.com>
2023-06-20 14:08:09 -0700

    6. There is no such thing as absolute value in this world

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * Prototype: int _abs(int)
+ * @b: the integer to print
+ *
+ * Return: 1 if success
+ *return 0 if false
+ */
+int _abs(int b)
+{
+
+	if (b >= 0)
+	{
+		return (b);
+	}
+	else if (b < 0)
+	{
+		return (-b);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar(10);
+}
+
